1eac580241e087be7b8440089ecbeee467d5bbd4
BILLTFI9 <146586280+BILLTFI9@users.noreply.github.com>
2025-02-25 23:55:08 +0200

    February 25 Commit 1

--- src/main/java/net/vasilis/cavernroot/world/gen/ModTreeGeneration.java
+++ src/main/java/net/vasilis/cavernroot/world/gen/ModTreeGeneration.java
@@ -9,7 +9,10 @@ import net.vasilis.cavernroot.world.ModPlacedFeatures;
 
 public class ModTreeGeneration {
     public static void generateTrees() {
-        BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.DRIPSTONE_CAVES, BiomeKeys.DEEP_DARK, BiomeKeys.PLAINS, BiomeKeys.FOREST),
+
+        // The following line is similar to cancer. It spreads and grows larger, but I cannot stop it.
+        BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.DRIPSTONE_CAVES, BiomeKeys.DEEP_DARK, BiomeKeys.PLAINS, BiomeKeys.FOREST, BiomeKeys.TAIGA, BiomeKeys.DESERT, BiomeKeys.BADLANDS),
                 GenerationStep.Feature.VEGETAL_DECORATION, ModPlacedFeatures.CAVERN_ROOT_PLACED_KEY);
+
     }
 }
