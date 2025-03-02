d3015bd7051f76c45866af7803bc6055bc771b7d
BILLTFI9 <146586280+BILLTFI9@users.noreply.github.com>
2025-03-02 09:06:30 +0200

    Empty master branch

--- src/main/java/net/vasilis/cavernroot/world/gen/ModWorldGeneration.java
+++
@@ -1,9 +0,0 @@
-package net.vasilis.cavernroot.world.gen;
-
-public class ModWorldGeneration {
-    public static void generateModWorldGen() {
-
-        ModTreeGeneration.generateTrees();
-
-    }
-}
